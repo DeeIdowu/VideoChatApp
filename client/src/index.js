06c616b41466a93c22d27167c86b95a7b9c8c2c7
Dennis Idowu <dennis_idowu@live.com>
2021-04-06 15:16:24 -0400

    wrapped react-app in context provider via index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {ContextProvider} from './SocketContext';
+import {ContextProvider} from 'SocketContext';
 import App from './App';
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ContextProvider>
     <App />
-  </React.StrictMode>,
+  </ContextProvider>,
   document.getElementById('root')
 );
 
