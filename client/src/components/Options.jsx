44103dc37c004e6c0b5a109d79e4daf8280c1467
Dennis Idowu <dennis_idowu@live.com>
2021-04-06 19:55:41 -0400

    imported useState

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import {Button, TextField, Grid, Typography, Container, Paper} from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
@@ -38,7 +38,9 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const Options = ({children}) => {
-    const context = useContext(SocketContext);
+    const {me, callAccepted, name, setName, callEnded, leaveCall, callUser} = useContext(SocketContext);
+    const [idToCall, setIdToCall] = useState('');
+
     return (
         <div>
            Options
