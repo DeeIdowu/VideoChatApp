cfa9b32acf74ee7c11b18619cf985f1446ba2d81
Dennis Idowu <dennis_idowu@live.com>
2021-04-06 12:38:09 -0400

    working on styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Typography, AppBar} from '@material-ui/core';
+
 import VideoPlayer from './components/VideoPlayer';
 import Notifications from './components/Notifications';
 import Options from './components/Options';
