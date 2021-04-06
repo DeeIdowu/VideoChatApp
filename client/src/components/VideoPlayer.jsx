b0b009f4202a020be7d9711d73e4e9254ab4330c
Dennis Idowu <dennis_idowu@live.com>
2021-04-06 15:49:28 -0400

    imported makeStyles and SocketContext into VideoPlayer

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import {Grid, Typography, Paper} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+
+import {SocketContext} from '../SocketContext';
+
 
 const VideoPlayer = () => {
     return (
