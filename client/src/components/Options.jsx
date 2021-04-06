10237ead0c2927405ab7aff84ca1dacf0b30a4e7
Dennis Idowu <dennis_idowu@live.com>
2021-04-06 12:25:02 -0400

    passed children into Options component

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Options = ({children}) => {
     return (
         <div>
-            <h1> Options </h1>
+           Options
+           {children}
         </div>
     )
 }
