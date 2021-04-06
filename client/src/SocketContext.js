fc4782897d401fa43755c44f69943805752438a1
Dennis Idowu <dennis_idowu@live.com>
2021-04-06 14:14:47 -0400

    setting up communication of socket-io from client side to server-side via SocketContext.js

---
+++ client/src/SocketContext.js
@@ -0,0 +1,7 @@
+import React, {createContext, useState, useRef, useEffect} from 'react';
+import {io} from 'socket.io-client';
+import Peer from 'simple-peer';
+
+const SocketContet = createContext();
+
+const socket = io("http://localhost:5000");
