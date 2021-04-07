8d836cef6a0b5670da37b3978f5708c97acdb14f
Dennis Idowu <dennis_idowu@live.com>
2021-04-06 20:25:14 -0400

    editing Notifications component

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {Button} from '@material-ui/core';
+
+import {SocketContext} from '../SocketContext';
+
 
 const Notifications = () => {
     return (
